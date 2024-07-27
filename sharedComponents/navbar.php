c64abf026048356031ad7af1212fe2e1253267fb
Mazin Fouad <m.fouad@live.de>
2024-07-27 20:04:52 +0200

    refactor: Improve code structure and organization

--- sharedComponents/navbar.php
+++ sharedComponents/navbar.php
@@ -4,15 +4,17 @@
 
 <style>
     .navbar-nav .nav-link:hover {
-      color: #ffcc00 !important; /* Change to your desired hover color */
+      color: #ffcc00 !important; 
+      transition: all 225ms ease-in-out;
     }
     .navbar-nav .nav-link.active {
       color: #ff6600 !important; /* Change to your desired active color */
+      font-weight: bold;
+      scale: 1.1;
     }
     </style>
 
 <nav class="navbar navbar-expand-lg bg-dark">
- 
   <div class="container">
     <a class="navbar-brand " href="#"><img src="./assets/logo_transparent.png" alt="Logo" style="width: 200px; height: 80px;"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,9 +22,6 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav ms-auto">
-        <!-- <a class="nav-link text-white" href="index.php">Home</a>
-        <a class="nav-link text-white" href="notes.php">My Notes</a>
-        <a class="nav-link text-white" href="insert.php">Add Notes</a> -->
         <a class="nav-link text-white <?php echo $current_page == 'index.php' ? 'active' : ''; ?>" href="index.php">Home</a>
         <a class="nav-link text-white <?php echo $current_page == 'notes.php' ? 'active' : ''; ?>" href="notes.php">My Notes</a>
         <a class="nav-link text-white <?php echo $current_page == 'insert.php' ? 'active' : ''; ?>" href="insert.php">Add Notes</a>
@@ -30,4 +29,5 @@
     </div>
   </div>
 </nav>
+
 
