c1bcca600159e6c35967431c0e34897737382593
Mazin Fouad <m.fouad@live.de>
2024-07-29 17:16:04 +0200

    feat: Add form validation for noteTitle and priority inputs

--- form.php
+++ form.php
@@ -3,25 +3,25 @@ echo '
 <div class="w-25 h-75 mx-auto align-content-center" style="min-height: calc(100dvh - 156px);">
     <form action="notes.php" method="post" class="container">
         <div class="input-group flex-nowrap">
-            <input type="text" class="form-control" placeholder="Note Title" name="noteTitle">
+            <input type="text" class="form-control" placeholder="Note Title" name="noteTitle" required>
         </div>
    
 
  <div class="container mt-2 d-flex justify-content-around align-content-center">
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="priority" id="urgent" value="urgent">
+            <input class="form-check-input" type="radio" name="priority" id="urgent" value="urgent" required>
             <label class="form-check-label text-danger" for="urgent">
                 Urgent
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="priority" id="medium" value="medium">
+            <input class="form-check-input" type="radio" name="priority" id="medium" value="medium" required>
             <label class="form-check-label text-warning" for="medium">
                 Medium
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="priority" id="low" value="low">
+            <input class="form-check-input" type="radio" name="priority" id="low" value="low" required>
             <label class="form-check-label text-success" for="low">
                 Low
             </label>
@@ -29,7 +29,7 @@ echo '
     </div>
 
     <div class="form-floating mt-3">
-    <textarea class="form-control" placeholder="Leave a comment here" name="description" style="height: 200px; resize:none;"></textarea>
+    <textarea class="form-control" placeholder="Leave a comment here" name="description" style="height: 200px; resize:none;" required></textarea>
     <label for="floatingTextarea2">Description</label>
     </div>
 
