76d5d30d087ffc0abf06bc879bf0a1f17d1aa3bf
Mazin Fouad <m.fouad@live.de>
2024-07-29 17:41:27 +0200

    Refactor note card style and structure

--- hero.php
+++ hero.php
@@ -1,9 +1,7 @@
 <div class='bg-light p-5 rounded-lg bg-secondary'>
   <div class="container align-content-center" style="min-height: calc(100dvh - 252px);">
     <h1 class='display-4'>Welcome to Your Notes Hub!</h1>
-    <p class='lead'>Discover the easiest way to organize, manage, and access your notes with our streamlined platform.</p>
-    <hr class="hr-style">
-    <p>Our notes website offers intuitive tools and features designed to enhance your productivity and keep your information at your fingertips.</p>
+    <p class='lead'>Discover the easiest way to organize, manage, and access your notes <br> with our streamlined platform.</p>
     <a class='btn btn-dark' href='notes.php' role='button'>Get Started</a>
   </div>
 </div>
